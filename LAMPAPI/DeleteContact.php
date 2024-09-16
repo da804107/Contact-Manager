3b1c94ed82b1ba68f30802147a8635d80a054130
Daisy Hernandez Gonzalez <114943299+da804107@users.noreply.github.com>
2024-09-16 10:17:05 -0400

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,10 +1,17 @@
 <?php
 
 		// HOW TO SECURE THIS?
+		// Store user id and hashed password on client side 
+		// All operations need the user to send the user id and hashed password for verification
+		// Contact updating/deleting requests must belong to the user
 		
     $inData = getRequestInfo();
 
     $id = $inData["ID"];
+    $userID = $inData["UserID"];
+    $hash = $inData["Hash"];
+		
+		$validity = validateUser($userID, $hash);
 
     $conn = new mysqli("localhost", "daisy", "SPOoks0219!!", "SMALLPROJ");
     if($conn->connect_error)
@@ -12,12 +19,17 @@
         returnWithError( $conn->connect_error );
     }
     else{
-        $stmt = $conn->prepare("DELETE from Contacts WHERE ID=?");
-        $stmt->bind_param("s", $id);
+        $stmt = $conn->prepare("DELETE from Contacts WHERE ID=? and UserID=?");
+        $stmt->bind_param("ss", $id, $userID);
         $stmt->execute();
         $stmt->close();
         $conn->close();
         returnWithSuccess();
+    }
+		
+		function validateUser($userID, $hash)
+    {
+				return true;
     }
 
     function getRequestInfo()
