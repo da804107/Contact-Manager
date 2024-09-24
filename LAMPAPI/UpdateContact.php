169bf3323ee806898b1bcbb944054d69d22faa5d
Daisy Hernandez Gonzalez <114943299+da804107@users.noreply.github.com>
2024-09-24 10:50:12 -0400

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,10 +1,23 @@
 <?php
+
+		// HOW TO SECURE THIS?
+		// Store user id and hashed password on client side 
+		// All operations need the user to send the user id and hashed password for verification
+		// Contact updating/deleting requests must belong to the user
+		
     $inData = getRequestInfo();
 
+    $id = $inData["id"];
     $name = $inData["name"];
-    $phone = $inData["phone"];
     $email = $inData["email"];
-    $ID = $inData["id"];
+    $phone = $inData["phone"];
+
+    $userID = $inData["userId"];
+
+
+    //$hash = $inData["Hash"];
+		
+	//$validity = validateUser($userID, $hash);
 
     $conn = new mysqli("localhost", "daisy", "SPOoks0219!!", "SMALLPROJ");
     if($conn->connect_error)
@@ -12,12 +25,28 @@
         returnWithError( $conn->connect_error );
     }
     else{
-        $stmt = $conn->prepare("UPDATE Contacts (Name,Phone,Email) VALUES(?,?,?) WHERE ID=?");
-        $stmt->bind_param("ssss", $name, $phone, $email, $ID);
+        $stmt = $conn->prepare("Update Contacts set Name = ? WHERE ID=?");
+        $stmt->bind_param("ss", $name, $id);
         $stmt->execute();
+        $stmt->close();
+				
+				$stmt = $conn->prepare("Update Contacts set Phone = ? WHERE ID=?");
+        $stmt->bind_param("ss", $phone, $id);
+        $stmt->execute();
+        $stmt->close();
+				
+				$stmt = $conn->prepare("Update Contacts set Email = ? WHERE ID=?");
+        $stmt->bind_param("ss", $email, $id);
+        $stmt->execute();
+				
         $stmt->close();
         $conn->close();
         returnWithSuccess();
+    }
+		
+		function validateUser($userID, $hash)
+    {
+				return true;
     }
 
     function getRequestInfo()
@@ -31,6 +60,7 @@
         echo $obj;
     }
 
+    
     function returnWithError( $err )
     {
         $retValue = '{"result":"error","error":"' . $err . '"}';
