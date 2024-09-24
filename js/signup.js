3d9ce192eecfd809787827fd05bdaef6c90c85e5
Daisy Hernandez Gonzalez <114943299+da804107@users.noreply.github.com>
2024-09-24 10:46:15 -0400

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,14 +1,18 @@
 
 function doSignup() {
 	userId = 0;
-	let flag = true;
+	//let flag = true;
 		
-	let firstName = document.getElementById("firstname").value;
-	let lastName = document.getElementById("lastname").value;
+	firstName = document.getElementById("firstname").value;
+	lastName = document.getElementById("lastname").value;
 	let login = document.getElementById("username").value;
 	let password = document.getElementById("password").value;
 	
-	
+	if ( !(firstName && lastName && login && password) ) {
+		showToast("Please fill in all fields");
+		console.log("Blank!");
+		return;
+	}	
 
 	let tmp = { firstName: firstName, lastName: lastName, login: login, password: password };
 	let jsonPayload = JSON.stringify(tmp);
@@ -21,12 +25,27 @@ function doSignup() {
 	try {
 		xhr.onreadystatechange = function () {
 			if (this.readyState == 4 && this.status == 200) {
+
+				let jsonObject = JSON.parse( xhr.responseText );
+
+				if(jsonObject.error && jsonObject.error === "Login already exists."){
+					showToast("Username already exists! Please create a different one.");
+					return;
+				}
+
+				userId = jsonObject.id;
+
+				console.log(userId);
+
+				saveCookie();
+				
 				window.location.href = "Landingpage.html";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch (err) {
+		console.log(err);
 	}
 
 }
